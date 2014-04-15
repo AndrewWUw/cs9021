54c9a5400a522d32393cf8b533a95a9703885676
AndrewWUw <andrew_wu@outlook.com>
2014-04-15 20:23:05 +1000

    test

--- quizes/quiz5.c
+++ quizes/quiz5.c
@@ -14,7 +14,17 @@
 #define LEFT -1
 #define RIGHT 1
 
-bool grid[DIM][DIM];
+bool grid[DIM][DIM] = {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
 void display_grid(void);
 int size_of_largest_parallelogram(void);
@@ -23,44 +33,43 @@ int size_of_largest_parallelogram(void);
  * having this row of 1s as top side, and with vertical sides going down in 1 of 3 possible
  * directions as selected by dir. */
 int size_of_largest_parallelogram_with_given_top_side(int i, int j1, int j2, int dir);
-int size_of_largest_parallelogram_with_given_top_side_STRAIGHT(int i1, int j1, int j2);
-int size_of_largest_parallelogram_with_given_top_side_LEFT(int i1, int j1, int j2);
-int size_of_largest_parallelogram_with_given_top_side_RIGHT(int i1, int j1, int j2);
+int find_longest_one(int i, int j);
+int find_largest_size_in_one_position(int i, int j, int length);
+
 int main(int argc, char **argv) {
     /* We run the program with two command line arguments. */
-    if (argc != 3) {
-        printf("Please provide 2 command line arguments.\n");
-        return EXIT_FAILURE;
-    }
-    /* The first one will determine our probability distribution;
-     * it is meant to be nonzero. */
-    int nb_of_possible_outcomes = strtol(argv[1], NULL, 10); // convert string to long int
-    if (!nb_of_possible_outcomes) {
-        printf("The first command line argument should not be 0.\n");
-        return EXIT_FAILURE;
-    }
-    /* We use the second command line argument, meant to be a nonnegative integer,
-     * as a seed for the random number generator. */
-    srand(strtoul(argv[2], NULL, 10));
-    /* We fill the grid with randomly generated 0s and 1s,
-     * with for every cell, a probability of 1/nb_of_possible_outcomes to generate a 0. */
-    if (nb_of_possible_outcomes > 0)
-        for (int i = 0; i < DIM; ++i)
-            for (int j = 0; j < DIM; ++j)
-                grid[i][j] = rand() % nb_of_possible_outcomes > 0;
-    else {
-        for (int i = 0; i < DIM; ++i)
-            for (int j = 0; j < DIM; ++j)
-                grid[i][j] = rand() % nb_of_possible_outcomes == 0;
-    }
+//    if (argc != 3) {
+//        printf("Please provide 2 command line arguments.\n");
+//        return EXIT_FAILURE;
+//    }
+//    /* The first one will determine our probability distribution;
+//     * it is meant to be nonzero. */
+//    int nb_of_possible_outcomes = strtol(argv[1], NULL, 10); // convert string to long int
+//    if (!nb_of_possible_outcomes) {
+//        printf("The first command line argument should not be 0.\n");
+//        return EXIT_FAILURE;
+//    }
+//    /* We use the second command line argument, meant to be a nonnegative integer,
+//     * as a seed for the random number generator. */
+//    srand(strtoul(argv[2], NULL, 10));
+//    /* We fill the grid with randomly generated 0s and 1s,
+//     * with for every cell, a probability of 1/nb_of_possible_outcomes to generate a 0. */
+//    if (nb_of_possible_outcomes > 0)
+//        for (int i = 0; i < DIM; ++i)
+//            for (int j = 0; j < DIM; ++j)
+//                grid[i][j] = rand() % nb_of_possible_outcomes > 0;
+//    else {
+//        for (int i = 0; i < DIM; ++i)
+//            for (int j = 0; j < DIM; ++j)
+//                grid[i][j] = rand() % nb_of_possible_outcomes == 0;
+//    }
+
     puts("Here is the grid that has been generated:\n");
     display_grid();
-    int size = size_of_largest_parallelogram();
 
+    int size = size_of_largest_parallelogram();
     if (size)
-        printf(
-                "The largest parallelogram with horizontal sides has a size of %d.\n",
-                size_of_largest_parallelogram());
+        printf("The largest parallelogram with horizontal sides has a size of %d.\n", size_of_largest_parallelogram());
     else
         printf("There is no parallelogram with horizontal sides.\n");
     return EXIT_SUCCESS;
@@ -80,30 +89,24 @@ int size_of_largest_parallelogram(void) {
     int sizeArray[DIM] = { 0 };
     for (int i = 0; i < DIM; i++) {
         int size = 0;
-        int j1 = 0;
-        int j2 = 0;
-
-        // find the longest line in a row
-
         for (int j = 0; j < DIM; j++) {
-            int length = 0;
+            // for every position that grid[i][j] == 1
+            // find the longest length of 1s,
+            // then try to find the largest size in this position
+        	// in the last, save the largest size in this line to sizeArray[i]
+        	int length = 0;
+        	int sizeInLine = 0;
             if (grid[i][j] == 1) {
-                length++;
-                if (j1 == 0) {
-                    j1 = j;
-                }
-                j2 = j;
+            	int temp = 0;
+            	length = find_longest_one(i, j);
+				temp = find_largest_size_in_one_position(i, j, length);
+				if(temp > sizeInLine) {
+					sizeInLine = temp;
+				}
+            	j += length;
             } else {
                 length = 0;
-                j1 = 0;
-                j2 = 0;
             }
-        }
-        size = size_of_largest_parallelogram_with_given_top_side(i, j1, j2, STRAIGHT);
-        if (size < size_of_largest_parallelogram_with_given_top_side(i, j1, j2, LEFT)) {
-            size = size_of_largest_parallelogram_with_given_top_side(i, j1, j2, LEFT);
-        } else if (size < size_of_largest_parallelogram_with_given_top_side(i, j1, j2, RIGHT)) {
-            size = size_of_largest_parallelogram_with_given_top_side(i, j1, j2, RIGHT);
         }
         sizeArray[i] = size;
     }
@@ -120,39 +123,53 @@ int size_of_largest_parallelogram(void) {
 int size_of_largest_parallelogram_with_given_top_side(int i1, int j1, int j2, int dir) {
 
     int size = j2 - j1 + 1;
-    for (int i = i1; i < DIM; i++) {
-        for (int j = j1; j <= j2; j + dir) {
+//	for (int j = j1; j <= j2; ++j) {
+//		if (grid[i1][j] != 1) {
+//			return 0;
+//		}
+//	}
 
-
-
-
-            switch (dir) {
-            case STRAIGHT:
-
-                break;
-            case LEFT:
-
-                break;
-            case RIGHT:
-
-                break;
-            }
+	switch (dir) {
+	case STRAIGHT:
+        if (i1 < DIM -1) {
+            size = size + size_of_largest_parallelogram_with_given_top_side(i1 + 1, j1, j2, STRAIGHT);
         }
+		break;
+	case LEFT:
+        if (i1 < DIM - 1 && j1 > 0) {
+            size = size + size_of_largest_parallelogram_with_given_top_side(i1 + 1, j1 - 1, j2 - 1, LEFT);
+        }
+		break;
+	case RIGHT:
+        if (i1 < DIM - 1 && j2 < 9) {
+            size = size + size_of_largest_parallelogram_with_given_top_side(i1 + 1, j1 + 1, j2 + 1, RIGHT);
+        }
+		break;
+	}
+    return size;
+
+}
+
+int find_longest_one(int i, int j) {
+	int length = 1;
+	for (int k = j; k < DIM; k++) {
+		if (grid[i][k] == 1) {
+			length++;
+		} else
+			break;
+	}
+	return length;
+}
+
+int find_largest_size_in_one_position(int i, int j, int length) {
+	int size = 0;
+
+    size = size_of_largest_parallelogram_with_given_top_side(i, j, j + length, STRAIGHT);
+    if (size < size_of_largest_parallelogram_with_given_top_side(i, j, j + length, LEFT)) {
+        size = size_of_largest_parallelogram_with_given_top_side(i, j, j + length, LEFT);
+    } else if (size < size_of_largest_parallelogram_with_given_top_side(i, j, j + length, RIGHT)) {
+        size = size_of_largest_parallelogram_with_given_top_side(i, j, j + length, RIGHT);
     }
 
-
-    return 0;
-
-}
-
-int size_of_largest_parallelogram_with_given_top_side_STRAIGHT(int i1,  int j1, int j2) {
-    return 0;
-}
-
-int size_of_largest_parallelogram_with_given_top_side_LEFT(int i1, int j1, int j2) {
-    return 0;
-}
-
-int size_of_largest_parallelogram_with_given_top_side_RIGHT(int i1, int j1, int j2) {
-    return 0;
+    return size;
 }
