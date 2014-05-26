76b77c900c3411f873ecb0efa036a52cfe1d1ec4
AndrewWUw <andrew_wu@outlook.com>
2014-05-26 21:44:30 +1000

    init for quiz10

---
+++ quizes/Material_quiz10/my_code.c
@@ -0,0 +1,28 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Description: Takes +s and digits as command line arguments, checks whether  *
+ *              they make up a valid infix expression, and evaluates it        *
+ *              in case it is.                                                 *
+ *                                                                             *
+ * Written by ***         for COMP9021                                         *
+ *                                                                             *
+ * Other source files, if any, one per line, starting on the next line:        *
+ *       tailored_stack.c                                                      *
+ *       quiz10.c                                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* THIS IS THE ONLY FILE YOU HAVE TO SUBMIT.
+ * IT WILL BE COMPILED WITH THE OTHER FILES YOU HAVE BEEN PROVIDED WITH. */
+
+#include "my_code.h"
+
+void store_command_line_args_in_stack(int argc, char **argv, Stack *const pt_to_stack) {
+    /* REPLACE WITH COMMENT WITH YOUR CODE */
+}
+
+bool is_correct_prefix_expression(Stack *const pt_to_stack) {
+    /* REPLACE WITH COMMENT WITH YOUR CODE */
+}
+
+Value evaluate_expression(Stack *const pt_to_stack) {
+    /* REPLACE WITH COMMENT WITH YOUR CODE */
+}
